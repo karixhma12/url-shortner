dd99980020940c8432e3f7f3f05ee8670fc62e2d
Karishma <karishma.7022@gmail.com>
2026-06-05 12:26:23 +0530

    feat : signin route with JWT

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const {signupSchema} = require("../validators");
+const jwt = require("jsonwebtoken");
 
 router.use(express.json());
 
@@ -39,6 +40,40 @@ router.post("/signup",async function(req,res){
     else{
         return res.status(401).json({message : "A user account with that email-id already exists!"});
     }
+})
+
+
+router.post("/signin",async function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //step 1 : validate input with zod 
+    try{
+        signupSchema.parse(req.body)
+    }
+    catch(err){
+        return res.status(401).json({message : err});
+    }
+
+    //step 2: find user by email, if not found - return an error 
+    const user = await User.findOne({
+        email : email
+    })
+
+    if(!user){
+        return res.status(401).json({message : "No such user found!"});
+    }
+
+    //step 3 : compare passwords 
+    const isValidPassword = await bcrypt.compare(password,user.password);
+    if(!isValidPassword){
+        return res.status(401).json({message : "Incorrect password"});
+    }
+
+    //step 4 : generate jwt and send it back 
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+    res.status(200).json({message : "successfully signed in!", token : token})
+
 
 })
 
