d670731c46fb324dee681f7dc1622a08fae4c767
Karishma <karishma.7022@gmail.com>
2026-06-08 13:14:58 +0530

    feat : dashboard with URL list

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import {useState,useContext,useRef} from "react";
+import {useState,useEffect,useContext,useRef} from "react";
 import { AuthContext } from "../context/AuthContext";
 import axios from "axios";
 
@@ -6,6 +6,16 @@ function Dashboard(){
     const inputRef = useRef("");
     const [shortCode,setShortCode] = useState("");
     const {token} = useContext(AuthContext);
+
+    const [myUrls,setMyUrls] = useState([]);
+
+    useEffect(()=>{
+        async function fetchUrls(){
+            const response = await axios.get("http://localhost:3000/api/url/my-urls",{headers:{Authorization:`Bearer ${token}`}});
+            setMyUrls(response.data.shortCodes);
+        } 
+        fetchUrls();
+    },[])
 
     async function shortenUrl(){
        const response = await axios.post("http://localhost:3000/api/url/shorten",{originalUrl : inputRef.current.value},{headers:{Authorization:`Bearer ${token}`}});
@@ -17,7 +27,16 @@ function Dashboard(){
             <input ref={inputRef}></input>
             <button onClick={()=>{shortenUrl()}}> Submit </button>
             {shortCode ? <div>{shortCode}</div> : <div></div>}
+            <div>
+                {myUrls.map((url)=>{
+                    return <div key={url._id}>
+                        <span>{url.originalUrl}</span> →
+                        <a href={`http://localhost:3000/${url.shortCode}`}>{url.shortCode}</a>
+                    </div>
+                })}
+            </div>
         </div>
+        
     )
 }
 
