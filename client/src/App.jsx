9691c02a056fe70e7ffcbbf8d3e857ea5b5e8d49
Karishma <karishma.7022@gmail.com>
2026-06-08 15:20:17 +0530

    feat:protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {Route,Routes} from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App(){
@@ -9,7 +10,11 @@ function App(){
     <Routes>
       <Route path="/signup" element={<Signup/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
-      <Route path="/dashboard" element={<Dashboard/>}></Route>
+      <Route path="/dashboard" element={
+        <ProtectedRoute>
+          <Dashboard/>
+        </ProtectedRoute>
+        }></Route>
     </Routes>
   )
  
